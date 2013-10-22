2d0709793d9d3d73361b011945aad9b9e2710519
vssystemluba <mixter.c@gmail.com>
2013-10-22 17:52:26 -0600

    updated karma conf file

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -47,7 +47,9 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: [ 'PhantomJS',
+    			'Chrome',
+    			'Firefox'],
 
 
     // Continuous Integration mode
